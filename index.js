fb3d1063c8732501460f72fddd218c43c614a5e1
Sadhika1012 <111134246+Sadhika1012@users.noreply.github.com>
2023-02-16 13:21:17 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const express  = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const pinRoute = require('./routes/pins');
 
 dotenv.config();
 
 const app = express();
-
+app.use(cors()); 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL, {useNewUrlParser:true},{useUnifiedTopology:true}).then(
+mongoose.connect("mongodb://localhost:27017/Hospital", {useNewUrlParser:true},{useUnifiedTopology:true}).then(
     () => {
         console.log("MongoDB connection successful");
+        const db = mongoose.connection;
+        db.once("open", () => {
+            console.log("MongoDB database connection established successfully");
+            db.pins.createIndex({ loc: "2dsphere" });
+        });
     }
 ).catch((err) => console.log(err));
+
 
 app.use('/api/pins', pinRoute);
 
